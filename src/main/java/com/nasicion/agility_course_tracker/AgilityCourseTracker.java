c3b3a6b3d90cb0d1831f485e40cee221fe94cd58
GLOBANT\guillermo.nasi <guillermo.nasi@globant.com>
2017-05-24 18:02:10 -0300

    Setting up application

--- src/main/java/com/nasicion/agility_course_tracker/AgilityCourseTracker.java
+++ src/main/java/com/nasicion/agility_course_tracker/AgilityCourseTracker.java
@@ -1,34 +1,62 @@
 package com.nasicion.agility_course_tracker;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * Created by guillermo.nasi on 23/05/2017.
  */
 @SpringBootApplication()
 public class AgilityCourseTracker {
+
     public static void main(String[] args) {
         SpringApplication.run(AgilityCourseTracker.class, args);
+        openBrowser();
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
+    /**
+     * Starts a web browser in the host machine
+     */
+    private static void openBrowser() {
+        String url = "http://localhost:8080/";
 
-//            System.out.println("Let's inspect the beans provided by Spring Boot:");
+        String os = System.getProperty("os.name").toLowerCase();
+        Runtime rt = Runtime.getRuntime();
+        try{
 
-//            String[] beanNames = ctx.getBeanDefinitionNames();
-//            Arrays.sort(beanNames);
-//            for (String beanName : beanNames) {
-//                System.out.println(beanName);
-//            }
+            if (os.indexOf( "win" ) >= 0) {
 
-        };
+                // this doesn't support showing urls in the form of "page.html#nameLink"
+                rt.exec( "rundll32 url.dll,FileProtocolHandler " + url);
+
+            } else if (os.indexOf( "mac" ) >= 0) {
+
+                rt.exec( "open " + url);
+
+            } else if (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0) {
+
+                // Do a best guess on unix until we get a platform independent way
+                // Build a list of browsers to try, in this order.
+                String[] browsers = {"epiphany", "firefox", "mozilla", "konqueror",
+                        "netscape","opera","links","lynx"};
+
+                // Build a command string which looks like "browser1 "url" || browser2 "url" ||..."
+                StringBuffer cmd = new StringBuffer();
+                for (int i=0; i<browsers.length; i++)
+                    cmd.append( (i==0  ? "" : " || " ) + browsers[i] +" \"" + url + "\" ");
+
+                rt.exec(new String[] { "sh", "-c", cmd.toString() });
+
+            } else {
+                return;
+            }
+        }catch (Exception e){
+            return;
+        }
+        return;
     }
 }
--- src/main/java/com/nasicion/agility_course_tracker/controller/MainController.java
+++ src/main/java/com/nasicion/agility_course_tracker/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.nasicion.agility_course_tracker.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by guillermo.nasi on 23/05/2017.
@@ -10,9 +13,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    @RequestMapping("/main")
-    public @ResponseBody String mainController(){
-        System.out.println("main controller");
-        return "Agility Course Helper";
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name+" moron");
+        return "greetings";
     }
 }
